5b5fd13698e872276a89f3bd786ee2410ec4c3ff
lalitk1997 <lalit.k1997@gmail.com>
2022-02-07 18:41:34 +0530

    Javascript-Level 1 Basics

---
+++ level_one_Basics(Javascript)/2_Part3.js
@@ -0,0 +1,4 @@
+var pounds = prompt("Enter weight in pounds !");
+var kilograms = pounds * 0.454;
+alert("Weight in kilograms is "+kilograms);
+console.log(kilograms);
---
+++ level_one_Basics(Javascript)/3_Part4.js
@@ -0,0 +1,29 @@
+console.log("Javascript working !");
+
+// hello world print 5 times using console.log
+for (var i = 0; i < 5; i++) {
+  console.log("Hello!");
+}
+
+console.log("Break!");
+//while loop
+var i=0
+while(i < 5){
+  console.log("Hello!");
+  i++;
+}
+
+// odd numbers
+var i=0;
+for (var i = 0; i < 10; i++) {
+  if(i % 2 == 1){
+    console.log(i);
+  }
+}
+ var i = 0;
+ while(i < 10){
+   if(i % 2 == 1){
+     console.log(i);
+   }
+   i++;
+ }
